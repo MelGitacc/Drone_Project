e1fc516dbf764a48deec035438cfda36136fd84c
MelGitacc <Emgeexy@yahoo.com>
2018-02-11 18:53:19 +0000

    Control Commands v2

--- app/src/main/java/com/example/dronepet/MainActivity.java
+++ app/src/main/java/com/example/dronepet/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.dronepet;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.parrot.arsdk.ARSDK;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentTransaction fragmentTransaction;
+    private FragmentManager fragmentManager;
     static {
         ARSDK.loadSDKLibs();
     }
@@ -15,5 +23,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    /*
+       perform Onclick listener event for the take off button
+        */
+    public void handleFragment(View view) {
+
+        Fragment selectedFragment;
+        if(view == findViewById(R.id.FloatingBttnTakeOff)){
+         selectedFragment = new ControlsFragment();
+         fragmentManager = getFragmentManager();
+         fragmentTransaction = fragmentManager.beginTransaction();
+         fragmentTransaction.replace(R.id.fragment, selectedFragment);
+         fragmentTransaction.commit();
+
+        }else if(view == findViewById(R.id.FloatingBttnMic)){
+
+        }else if( view == findViewById(R.id.FloatingBttnFollowMe)){
+
+        }
     }
 }
+
+
