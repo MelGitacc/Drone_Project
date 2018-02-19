146312db118c84972318e10a5c53a7ac02f098ce
MelGitAcc <emgeexy@yahoo.com>
2018-02-19 20:52:29 +0000

    takeOff Layout complete

--- app/src/main/java/com/example/dronepet/MainActivity.java
+++ app/src/main/java/com/example/dronepet/MainActivity.java
@@ -4,9 +4,11 @@ package com.example.dronepet;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.parrot.arsdk.ARSDK;
 
@@ -26,23 +28,30 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     /*
        perform Onclick listener event for the take off button
         */
     public void handleFragment(View view) {
 
         Fragment selectedFragment;
-        if(view == findViewById(R.id.FloatingBttnTakeOff)){
-         selectedFragment = new ControlsFragment();
-         fragmentManager = getFragmentManager();
-         fragmentTransaction = fragmentManager.beginTransaction();
-         fragmentTransaction.replace(R.id.fragment, selectedFragment);
-         fragmentTransaction.commit();
 
-        }else if(view == findViewById(R.id.FloatingBttnMic)){
+        if( view == findViewById(R.id.FloatingBttnFollowMe)){
+            Toast.makeText(getApplicationContext(), "follow me is selected", Toast.LENGTH_LONG).show();
 
-        }else if( view == findViewById(R.id.FloatingBttnFollowMe)){
+        }else if(view == findViewById(R.id.FloatingBttnTakeOff)){
+            selectedFragment = new ControlsFragment();
+            fragmentManager = getFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, selectedFragment);
+            fragmentTransaction.commit();
 
+
+            Toast.makeText(getApplicationContext(), "take off is selected", Toast.LENGTH_LONG).show();
+
+        }else if(view == findViewById(R.id.FloatingBttnMic)) {
+
+            Toast.makeText(getApplicationContext(), "Mic is selected", Toast.LENGTH_LONG).show();
         }
     }
 }
